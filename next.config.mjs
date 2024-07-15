8604992568b007a5f685e11ebeb6aa6d1981b6c7
MehKad <mehkadiri@gmail.com>
2024-07-15 13:59:50 +0100

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/mentory", // Replace with your GitHub repository name
+  assetPrefix: "/mentory/", // Replace with your GitHub repository name
+};
 
 export default nextConfig;
